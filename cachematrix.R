e544a35da4ad5c41307f5a94fa82c609860286a7
Dmitry Karasev <afterwhy@gmail.com>
2017-11-19 18:12:32 +0300

    Implemented makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Script contains functions to cache matrix inversion
 
-## Write a short comment describing this function
+## Creates wrapper object for matrix, which contains 4 functions:
+## get() - get original matrix
+## set() - set new matrix
+## setSolve() - set inversed matrix
+## getSolve() - get stored inversed matrix
+## 
+## Args:
+##   x: Matrix to store. Default is empty matrix with single NA value.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() x
+    setSolve <- function(solve) s <<- solve
+    getSolve <- function() s
+    list(set = set, get = get,
+         setSolve = setSolve,
+         getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+## Calculates inverted matrix or resurns previously calculated (cached) value
+## 
+## Args:
+##   x: Matrix object, created using method makeCacheMatrix().
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    s <- x$getSolve()
+    if(!is.null(s)) {
+        message("getting cached data")
+        return(s)
+    }
+    data <- x$get()
+    s <- solve(data)
+    x$setSolve(s)
+    s
 }
